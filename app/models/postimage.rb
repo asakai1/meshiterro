05557a959f37990002b504d36a4ee06b0fc10aaa
asakai1 <127031557+asakai1@users.noreply.github.com>
2023-04-05 21:40:01 +0000

    [Update]model

--- app/models/postimage.rb
+++ app/models/postimage.rb
@@ -1,2 +1,5 @@
 class Postimage < ApplicationRecord
+
+  has_one_attached :image
+
 end
--- db/migrate/20230404140558_create_postimages.rb
+++ db/migrate/20230404140558_create_postimages.rb
@@ -3,6 +3,9 @@ class CreatePostimages < ActiveRecord::Migration[6.1]
     create_table :postimages do |t|
 
       t.timestamps
+      t.string :shop_name
+      t.text :caption
+      t.integer :user_id
     end
   end
 end
