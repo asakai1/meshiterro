f3fc9c3240f96b99eb396b888d6a0549680d74ad
asakai1 <127031557+asakai1@users.noreply.github.com>
2023-04-06 21:43:36 +0000

    [Add]associate

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'hones#top'
   devise_for :users
+
+  resources :postimages, only: [:new, :index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-root to: "homes#top"
 get 'homes/about' => 'homes#about', as: 'about'
 end
